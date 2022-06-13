13ce5fa64fa476dc69c429df55f884fd1e42b8e7
Steeven Engracia <saengracia@espe.edu.ec>
2022-06-13 09:40:06 -0500

    Se añade el método menec

--- ventilador.py
+++ ventilador.py
@@ -19,3 +19,18 @@ class ventilador:
                         Indica si el ventilador se encuentra conectado.
         """
         self.aencendido=encendido
+
+    def menen(self):
+        """Detallal los parámetros de encendido al encender el ventilador
+
+            Parámetros
+            ----------
+            No contiene parámetros
+        
+        """
+        if not self.aencendido:
+            print("Encendiendo ventilador...")
+            print("Preparando motores")
+            print("Se ha encendido sin ningún problema el ventilador")
+        else:
+            print("El ventilador ya está encendido")
