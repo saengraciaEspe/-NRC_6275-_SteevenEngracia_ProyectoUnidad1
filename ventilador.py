9be6200d347d8ae9845f35d8eb9f7e331190020b
Steeven Engracia <saengracia@espe.edu.ec>
2022-06-13 09:35:13 -0500

    Se agrega la clase ventilador

---
+++ ventilador.py
@@ -0,0 +1,12 @@
+class ventilador:
+    """
+    Clase que representa un ventilador inteligente.
+    ...
+    
+    Atributos
+    ---------
+    aencendido: boolean
+                Indica si el ventilador se encuentra conectado.
+    
+    
+    """
